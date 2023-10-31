d4f23a0231dcbc90a79de6c5b1612d400a7cba2d
Gabriel Robaina <49736266+gpr-indevelopment@users.noreply.github.com>
2023-10-30 23:37:46 -0300

    Tried (uncusuccessfully) to install fio through function code

--- gcf-shell/index.js
+++ gcf-shell/index.js
@@ -1,5 +1,7 @@
 const functions = require("@google-cloud/functions-framework");
+const { execSync } = require("child_process");
 
 functions.http("gcf-shell", (req, res) => {
-  res.send("Hello, World");
+  console.info("Received request:", req);
+  res.send(execSync("sudo apt-get update", { encoding: "utf-8" }));
 });
