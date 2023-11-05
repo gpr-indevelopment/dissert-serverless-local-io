e183fb9dd5f1dc9c31cd130627be4fa6c81ca397
Gabriel Robaina <49736266+gpr-indevelopment@users.noreply.github.com>
2023-11-05 17:58:24 -0300

    Add initial code for dissert-exp-orchestrator

---
+++ dissert-exp-orchestrator/src/main/java/io/github/gprindevelopment/dissertexporchestrator/DissertExpOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package io.github.gprindevelopment.dissertexporchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DissertExpOrchestratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DissertExpOrchestratorApplication.class, args);
+	}
+
+}
---
+++ dissert-exp-orchestrator/src/test/java/io/github/gprindevelopment/dissertexporchestrator/DissertExpOrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gprindevelopment.dissertexporchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DissertExpOrchestratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
