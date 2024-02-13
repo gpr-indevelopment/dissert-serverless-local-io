5968331724f326c910b0acc3ccdc0a47b0f5fffb
Gabriel Robaina <gpr.indevelopment@gmail.com>
2024-02-13 00:51:02 -0300

    Removed legacy data models

--- dissert-exp-orchestrator/src/main/java/io/github/gprindevelopment/dissertexporchestrator/dd/domain/DdExpRecordRepository.java
+++
@@ -1,6 +0,0 @@
-package io.github.gprindevelopment.dissertexporchestrator.dd.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DdExpRecordRepository extends JpaRepository<DdExpRecordEntity, Long> {
-}
--- dissert-exp-orchestrator/src/main/java/io/github/gprindevelopment/dissertexporchestrator/dd/domain/DdOperationErrorRepository.java
+++
@@ -1,6 +0,0 @@
-package io.github.gprindevelopment.dissertexporchestrator.dd.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DdOperationErrorRepository extends JpaRepository<DdOperationErrorEntity, Long> {
-}
