919bf68345eca3ec6913cfb28c6adc655e6f8a9a
Gabriel Robaina <49736266+gpr-indevelopment@users.noreply.github.com>
2024-05-02 22:21:53 -0300

    Change cv data structure to also contain system name in same dataframe

--- analysis/dissert-cv.R
+++ analysis/dissert-cv.R
@@ -5,20 +5,14 @@ source(file.path(dirname(curDir), "dissert-queries.R"))
 
 CV <- function(x) { sd(x) / mean(x) }
 
-calculateAllCvs = function(data) {
-  lambda_data = data[data$system_name == "LAMBDA_DD",]
-  gcf_data = data[data$system_name == "GCF_DD", ]
-  lambda_cvs = calculateGroupCvs(lambda_data);
-  gcf_cvs = calculateGroupCvs(gcf_data);
-  return(list("lambda" = lambda_cvs, "gcf" = gcf_cvs))
-}
-
 calculateGroupCvs = function(data) {
   resultDf = data.frame(
+    system_name = character(),
     resource_tier = character(),
     file_size_bytes = integer(),
     io_size_bytes = integer(),
     cv = numeric(),
+    observations = integer(),
     stringsAsFactors = FALSE
   );
   uniques = unique(data$group_id);
@@ -26,15 +20,17 @@ calculateGroupCvs = function(data) {
     group_data = data[data$group_id == uniques[group],]
     cv_result = CV(group_data$latency_seconds);
     newRow = data.frame(
+      system_name = c(head(group_data, 1)$system_name),
       resource_tier= c(head(group_data, 1)$resource_tier),
       file_size_bytes= c(head(group_data, 1)$file_size_bytes),
       io_size_bytes= c(head(group_data, 1)$io_size_bytes),
-      cv = cv_result
+      cv = cv_result,
+      observations = nrow(group_data)
     );
     resultDf = rbind(resultDf,newRow);
   }
   return (resultDf);
 }
 
-writes = calculateAllCvs(cvWriteQuery())
-reads = calculateAllCvs(cvReadQuery())
+writes = calculateGroupCvs(cvWriteQuery())
+reads = calculateGroupCvs(cvReadQuery())
