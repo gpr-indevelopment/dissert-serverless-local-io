36412c3756a4f1eca81314ffd0683fab11929227
Gabriel Robaina <gpr.indevelopment@gmail.com>
2024-05-12 00:18:38 -0300

    Add script for confidence intervals

--- analysis/dissert-conf-int.R
+++ analysis/dissert-conf-int.R
@@ -5,9 +5,7 @@ source(file.path(dirname(curDir), "dissert-queries.R"))
 
 conf_level = 0.95;
 
-maxFileWriteConfInt = function() {
-  data = maxFileMinMaxIoWriteQuery();
-  
+maxFileConfInt = function(data) {
   lambda_data = data[data$system_name == "LAMBDA_DD", ]
   gcf_data = data[data$system_name == "GCF_DD", ]
   
@@ -34,7 +32,7 @@ maxFileWriteConfInt = function() {
   
   ggplot(results, aes(x=provider, y = (lower + upper) / 2)) +
     geom_point() +
-    facet_grid(~io_size, labeller = labeller(io_size = c("512" = "512 B", "128000" = "128 KB"))) + 
+    facet_wrap(~io_size, labeller = labeller(io_size = c("512" = "512 B", "128000" = "128 KB")), scales="free_y") + 
     geom_errorbar(aes(ymin = lower, ymax = upper, colour = provider)) +
     theme_bw() + 
     theme(legend.position = "none") +
@@ -43,4 +41,39 @@ maxFileWriteConfInt = function() {
   
 }
 
-maxFileWriteConfInt()
+minFileConfInt = function(data) {
+  # Convert to ms
+  data$latency_seconds = data$latency_seconds*1000
+  lambda_data = data[data$system_name == "LAMBDA_DD", ]
+  gcf_data = data[data$system_name == "GCF_DD", ]
+  
+  lambda_data_min_res = lambda_data[lambda_data$resource_tier == "TIER_1", ]
+  lambda_data_max_res = lambda_data[lambda_data$resource_tier == "TIER_5", ]
+  
+  gcf_data_min_res = gcf_data[gcf_data$resource_tier == "TIER_1", ]
+  gcf_data_max_res = gcf_data[gcf_data$resource_tier == "TIER_5", ]
+  
+  result_lambda_min_res = t.test(lambda_data_min_res$latency_seconds, conf_level = conf_level)$conf.int
+  result_lambda_max_res = t.test(lambda_data_max_res$latency_seconds, conf_level = conf_level)$conf.int
+  
+  result_gcf_min_res = t.test(gcf_data_min_res$latency_seconds, conf_level = conf_level)$conf.int
+  result_gcf_max_res = t.test(gcf_data_max_res$latency_seconds, conf_level = conf_level)$conf.int
+  
+  results = data.frame(
+    provider = c("LAMBDA_DD", "LAMBDA_DD", "GCF_DD", "GCF_DD"),
+    resource_tier = c("TIER_1", "TIER_5", "TIER_1", "TIER_5"),
+    lower = c(result_lambda_min_res[1], result_lambda_max_res[1], result_gcf_min_res[1], result_gcf_max_res[1]),
+    upper = c(result_lambda_min_res[2], result_lambda_max_res[2], result_gcf_min_res[2], result_gcf_max_res[2])
+  )
+  
+  print(results)
+  
+  ggplot(results, aes(x=provider, y = (lower + upper) / 2)) +
+    geom_point() +
+    facet_wrap(~resource_tier, labeller = labeller(resource_tier = c("TIER_1" = "Tier 1", "TIER_5" = "Tier 5")), scales="free_y") + 
+    geom_errorbar(aes(ymin = lower, ymax = upper, colour = provider)) +
+    theme_bw() + 
+    theme(legend.position = "none") +
+    labs(x = "Provider", y = "Latency (ms)", colour = "Platform") +
+    scale_x_discrete(labels=c("GCF_DD"="GCF", "LAMBDA_DD"="AWS Lambda"))
+}
